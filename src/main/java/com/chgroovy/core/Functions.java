fcb8e31a5662403e44865cf9c910985d2448290e
Pieter12345 <P.J.S.Kools@student.tudelft.nl>
2021-06-17 00:34:15 +0200

    Expand whitespaces to tabs

--- src/main/java/com/chgroovy/core/Functions.java
+++ src/main/java/com/chgroovy/core/Functions.java
@@ -20,69 +20,69 @@ import groovy.lang.GroovyShell;
  */
 public class Functions {
 
-    @api
-    public static class groovy extends AbstractFunction{
+	@api
+	public static class groovy extends AbstractFunction{
 
-        public Class[] thrown() {
-            return new Class[]{
+		public Class[] thrown() {
+			return new Class[]{
 				CREPluginInternalException.class
-            };
-        }
+			};
+		}
 
-        public boolean isRestricted() {
-            return true;
-        }
+		public boolean isRestricted() {
+			return true;
+		}
 
-        public Boolean runAsync() {
-            return null;
-        }
+		public Boolean runAsync() {
+			return null;
+		}
 
-        public Mixed exec(Target t, Environment environment, Mixed... args) throws ConfigRuntimeException {
-            String script = args[0].val();
-            CArray env = new CArray(t);
-            CArray toReturn = new CArray(t);
-            if(args.length > 1){
-                env = Static.getArray(args[1], t);
-            }
-            if(args.length > 2){
-                toReturn = Static.getArray(args[2], t);
-            }
-            Binding binding = new Binding();
-            try{
-                for(String key : env.stringKeySet()){
-                    binding.setVariable(key, Construct.GetPOJO(env.get(key, t)));
-                }
-                GroovyShell shell = new GroovyShell(binding);
-                shell.evaluate(script);
-            } catch(Exception ex){
-                throw new CREPluginInternalException(ex.getMessage(), t);
-            }
-            CArray ret = CArray.GetAssociativeArray(t);
-            for(String key : toReturn.stringKeySet()){
-                Object var = binding.getVariable(toReturn.get(key, t).val());
-                ret.set(toReturn.get(key, t).val(), Construct.GetConstruct(var), t);
-            }
-            return ret;
-        }
+		public Mixed exec(Target t, Environment environment, Mixed... args) throws ConfigRuntimeException {
+			String script = args[0].val();
+			CArray env = new CArray(t);
+			CArray toReturn = new CArray(t);
+			if(args.length > 1){
+				env = Static.getArray(args[1], t);
+			}
+			if(args.length > 2){
+				toReturn = Static.getArray(args[2], t);
+			}
+			Binding binding = new Binding();
+			try{
+				for(String key : env.stringKeySet()){
+					binding.setVariable(key, Construct.GetPOJO(env.get(key, t)));
+				}
+				GroovyShell shell = new GroovyShell(binding);
+				shell.evaluate(script);
+			} catch(Exception ex){
+				throw new CREPluginInternalException(ex.getMessage(), t);
+			}
+			CArray ret = CArray.GetAssociativeArray(t);
+			for(String key : toReturn.stringKeySet()){
+				Object var = binding.getVariable(toReturn.get(key, t).val());
+				ret.set(toReturn.get(key, t).val(), Construct.GetConstruct(var), t);
+			}
+			return ret;
+		}
 
-        public String getName() {
-            return "groovy";
-        }
+		public String getName() {
+			return "groovy";
+		}
 
-        public Integer[] numArgs() {
-            return new Integer[]{1, 2, 3};
-        }
+		public Integer[] numArgs() {
+			return new Integer[]{1, 2, 3};
+		}
 
-        public String docs() {
-            return "array {script, environment, toReturn} Runs a groovy script. The script can set variables beforehand with the environment"
-                    + " variable, which should be an associative array mapping variable names to values. Arrays are not directly supported,"
-                    + " as everything is simply passed in as a string. Values can be returned from the script, by giving a list of named values"
-                    + " to toReturn, which will cause those values to be returned as a part of the associative array returned.";
-        }
+		public String docs() {
+			return "array {script, environment, toReturn} Runs a groovy script. The script can set variables beforehand with the environment"
+					+ " variable, which should be an associative array mapping variable names to values. Arrays are not directly supported,"
+					+ " as everything is simply passed in as a string. Values can be returned from the script, by giving a list of named values"
+					+ " to toReturn, which will cause those values to be returned as a part of the associative array returned.";
+		}
 
-        public Version since() {
-            return MSVersion.V3_3_1;
-        }
+		public Version since() {
+			return MSVersion.V3_3_1;
+		}
 
-    }
+	}
 }
